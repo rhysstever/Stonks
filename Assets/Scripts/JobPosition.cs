f9c09211be4e8499d5ae20b52fd2e848f364df90
Rhys Stever <38188371+rhysstever@users.noreply.github.com>
2021-02-20 12:51:28 -0500

    Added working a job to the game! The player can now click to earn an hourly wage and get a promotion if they click enough

---
+++ Assets/Scripts/JobPosition.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class JobPosition
+{
+	#region Fields
+	private string positionTitle;
+	private float hourlyPay;
+	private int currentClicks;
+	private int clicksToPromotion;
+	private float clickBuyOutCost;
+	private JobPosition nextPosition;
+	#endregion
+
+	#region Properties
+	public string PositionTitle { get { return positionTitle; } }
+	public float HourlyPay { get { return hourlyPay; } }
+	public int CurrentClicks { get { return currentClicks; } }
+	public int ClicksToPromotion { get { return clicksToPromotion; } }
+	public float ClickBuyOutCost { get { return clickBuyOutCost; } }
+	public JobPosition NextPosition { get { return nextPosition; } }
+	#endregion
+
+	#region Constructor
+	/// <summary>
+	/// Creates a job position the player can have
+	/// </summary>
+	/// <param name="positionTitle">The name of the position</param>
+	/// <param name="hourlyPay">The amount per click the player makes</param>
+	/// <param name="currentClicks">How many clicks the player has clicked as this job position</param>
+	/// <param name="clicksToPromotion">How many total clicks the player needs to click to move to the next job</param>
+	/// <param name="clickBuyOutCost">How much each click costs to be bought</param>
+	/// <param name="nextPosition">The next job after this current one</param>
+	public JobPosition(string positionTitle, float hourlyPay, int currentClicks, int clicksToPromotion, float clickBuyOutCost, JobPosition nextPosition)
+	{
+		this.positionTitle = positionTitle;
+		this.hourlyPay = hourlyPay;
+		this.currentClicks = currentClicks;
+		this.clicksToPromotion = clicksToPromotion;
+		this.clickBuyOutCost = clickBuyOutCost;
+		this.nextPosition = nextPosition;
+	}
+	#endregion
+
+	#region Methods
+	/// <summary>
+	/// Adds clicks to the current click count
+	/// </summary>
+	/// <param name="numClicks">The number of clicks being added</param>
+	public void AddClicks(int numClicks)
+	{
+		currentClicks += numClicks;
+	}
+	#endregion
+}
